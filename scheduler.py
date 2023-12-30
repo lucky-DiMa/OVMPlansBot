d644c156629002e4d2923bb88d68f3ddf0193011
Dunin Dmitry Andreevich <d.dunin1709@gmail.com>
2023-12-30 12:21:04 +0500

    3.1 Added holidays

--- scheduler.py
+++ scheduler.py
@@ -2,9 +2,10 @@ import asyncio
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
+from config import HOLIDAYS
 from cron import send_plans_table, mailing, send_notifications
 
-if __name__ == '__main__':
+if __name__ == '__main__' and not HOLIDAYS:
     scheduler = AsyncIOScheduler()
     scheduler.add_job(send_plans_table, 'cron', day_of_week='mon-fri', hour=12, minute=0, end_date='2025-10-13')
     scheduler.add_job(mailing, 'cron', day_of_week='mon-fri', hour=11, minute=40, end_date='2025-10-13')
